252933c0f1859bbdc246a7cb346a3daff5a67bd1
JoelJvegas <joel18gonzalez@outlook.com>
2022-12-31 16:06:56 -0500

    Repair POST of Send Email
    Repair POST of Send Email

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import gspread
-from flask import Flask , render_template , request
+from flask import Flask , render_template , request , redirect , url_for
 
 gc = gspread.service_account(filename='simple-flask-portfolio.json')
 sh = gc.open('simple-flask-portfolio')
@@ -8,17 +8,11 @@ sh = gc.open('simple-flask-portfolio')
 shProfile = sh.get_worksheet(0)
 shContact = sh.get_worksheet(1)
 
-# shEducation = sh.get_worksheet(2)
-# shExperience = sh.get_worksheet(3)
-
-
 app = Flask(__name__)
 
-@app.route('/' , methods=['POST','GET']  )
+@app.route('/' )
 def home():
 
-    if request.method == 'POST':
-        shContact.append_row([ request.form['name'], request.form['email'], request.form['message']])
 
     profile = {
         'about': shProfile.acell('B1').value,
@@ -26,12 +20,15 @@ def home():
         'interests': shProfile.acell('B3').value,
         'education': shProfile.acell('B4').value,
     }
+
     return render_template('index.html' , profile=profile)
 
-@app.route('/contact' )
+@app.route('/contact', methods=['POST','GET']  )
 def contact():
-    # contact = {
-    #     'name': shContact.acell('A1').value,
-    #     'email': shContact.acell('B1').value,
-    # }
+
+    if request.method == 'POST':
+        shContact.append_row([ request.form['name'], request.form['email'], request.form['message']])
+
+        return redirect(url_for('home'))
+
     return render_template('contact.html' )
